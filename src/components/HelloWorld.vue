32e09b92fca268de80bc7ed77a2274fc66eb60b5
Kenny <kenny.devex@gmail.com>
2020-06-24 16:07:48 -0100

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button @click="count++">count is: {{ count }}</button>
+  <p>Estefânio Pereira Silva</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
